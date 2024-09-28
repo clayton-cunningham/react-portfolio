d8ab122301f7f12e9c156094e37a017e5d0033d9
Clayton Cunningham <clayton.c.cunnigham@gmail.com>
2024-09-28 17:42:37 -0400

    Changes;; Restyling page header;; mobile friendly spacing

--- src/components/primary-widgets/resume/ResumeDetails.tsx
+++ src/components/primary-widgets/resume/ResumeDetails.tsx
@@ -13,87 +13,99 @@ export const ResumeDetails = () => {
 
 
     return (
-        <div className="resume">
-            <PageSection>
-                <div className="resume-header">
-                        <h1>Clayton Cunningham</h1>
-                        <h2>Full Stack Developer</h2>
-                </div>
-                <div className="description">
-                    <h3>
-                        Full Stack Developer at CoStar with experience programming solutions for C#, Javascript, Java, C, Python, JavaScript, HTML and Typescript 
-                        seeking a full time position where I can apply and expand my software development and problem solving skills.
-                    </h3>
-                </div>
-            </PageSection>
-            <PageSection>
-                <Row className="cert-row">
-                    <a href={certificationLinks.ckad}><img className="cert-badge" src="./certifications/ckad_badge.png" /></a>
-                    <a href={certificationLinks.ccp}><img className="cert-badge" src="./certifications/aws_ccp_badge.png" /></a>
-                    <a href={certificationLinks.saa}><img className="cert-badge" src="./certifications/aws_saa_badge.png" /></a>
-                </Row>
-            </PageSection>
-            <PageSection className="section-align">
-                <LeftAlign>
-                    <h1>Skills</h1>
-                </LeftAlign>
-                <div className="section-align-body">
-                    <Column className="skills-section">
+        <>
+            <div className="resume">
+                <PageSection className="page-header">
+                    <div className="section-align">
                         <LeftAlign>
-                            <h2>Langages:</h2>
-                        </LeftAlign>
-                        <DynamicRowList
-                            childrenList={languages.map(l => {
-                                    return {
-                                        id: l,
-                                        component: <p className="pill">{l}</p>,
-                                    }
-                                })}
-                        />
-                        <LeftAlign>
-                            <h2>Software:</h2>
-                        </LeftAlign>
-                        <DynamicRowList
-                            childrenList={software.map(s => {
-                                    return {
-                                        id: s,
-                                        component: <p className="pill">{s}</p>,
-                                    }
-                                })}
-                        />
-                    </Column>
-                </div>
-            </PageSection>
-            <PageSection className="section-align">
-                <LeftAlign>
-                    <h1>Experience</h1>
-                </LeftAlign>
-                <div className="section-align-body">
-                    <Grid
-                        className="grid-with-logos"
-                        cells={[
-                            ...resumeCompanies.map((c) => 
-                                {
-                                    return [
-                                        <ExperienceHeader id={c.id} company={c} />,
-                                        <ExperienceBody id={c.id} company={c} />
-                                    ]
-                                }
-                            ).reduce((acc, curr) => acc.concat(curr)),
-                            <Row>
-                                <div className="logo">
-                                        <img src="vt.png"/>
-                                </div>
-                            </Row>,
-                            <div>
-                                <p>B.S., Computer Science; May 2019 | Minor, Chinese</p>
-                                <p>Virginia Polytechnic Institute and State University - Blacksburg, VA</p>
-                                <p>Dean’s List 2016, 2017 & 2018 | GPA: 3.4</p>
+                            <div className="resume-header">
+                                <LeftAlign>
+                                    <h1>Hello, I'm</h1><h1 className="highlight">Clay</h1>
+                                </LeftAlign>
+                                <LeftAlign>
+                                    <h2>I'm a</h2><h2 className="highlight">full stack software developer</h2>
+                                </LeftAlign>
                             </div>
-                        ]}
-                    />
-                </div>
-            </PageSection>
-        </div>
+                        </LeftAlign>
+                        <LeftAlign>
+                            <h3 className="description">
+                                I enjoy programming at either end of the tech stack whether it's creating data models and optimizing database queries, 
+                                or creating fluid digital experiences.
+                                I've spent the past 5 years building my full stack experience, and am now
+                                seeking a full time position where I can apply and expand my software development and problem solving skills.
+                            </h3>
+                        </LeftAlign>
+                    </div>
+                    <Row className="cert-row">
+                        <a href={certificationLinks.ckad}><img className="cert-badge" src="./certifications/ckad_badge.png" /></a>
+                        <a href={certificationLinks.ccp}><img className="cert-badge" src="./certifications/aws_ccp_badge.png" /></a>
+                        <a href={certificationLinks.saa}><img className="cert-badge" src="./certifications/aws_saa_badge.png" /></a>
+                    </Row>
+                </PageSection>
+            </div>
+            <div className="resume">
+                <PageSection className="section-align">
+                    <LeftAlign>
+                        <h1>Skills</h1>
+                    </LeftAlign>
+                    <div className="section-align-body">
+                        <Column className="skills-section">
+                            <LeftAlign>
+                                <h2>Langages:</h2>
+                            </LeftAlign>
+                            <DynamicRowList
+                                childrenList={languages.map(l => {
+                                        return {
+                                            id: l,
+                                            component: <p className="pill">{l}</p>,
+                                        }
+                                    })}
+                            />
+                            <LeftAlign>
+                                <h2>Software:</h2>
+                            </LeftAlign>
+                            <DynamicRowList
+                                childrenList={software.map(s => {
+                                        return {
+                                            id: s,
+                                            component: <p className="pill">{s}</p>,
+                                        }
+                                    })}
+                            />
+                        </Column>
+                    </div>
+                </PageSection>
+                <PageSection className="section-align">
+                    <LeftAlign>
+                        <h1>Experience</h1>
+                    </LeftAlign>
+                    <div className="section-align-body">
+                        <Grid
+                            className="grid-with-logos"
+                            cells={[
+                                ...resumeCompanies.map((c) => 
+                                    {
+                                        return [
+                                            <ExperienceHeader id={c.id} company={c} />,
+                                            <ExperienceBody id={c.id} company={c} />
+                                        ]
+                                    }
+                                ).reduce((acc, curr) => acc.concat(curr)),
+                                <Row>
+                                    <div className="logo">
+                                            <img src="vt.png"/>
+                                    </div>
+                                </Row>,
+                                <div>
+                                    <p>B.S., Computer Science; May 2019 | Minor, Chinese</p>
+                                    <p>Virginia Polytechnic Institute and State University - Blacksburg, VA</p>
+                                    <p>Dean’s List 2016, 2017 & 2018 | GPA: 3.4</p>
+                                </div>
+                            ]}
+                        />
+                    </div>
+                </PageSection>
+            </div>
+        </>
     )
 }
